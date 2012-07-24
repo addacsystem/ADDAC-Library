5189adb469fdf38246e77bcd427335eac92e51f5
joaoFonseca <joaodafonseca88@gmail.com>
2012-07-24 18:05:06 +0100

    Renamed Liss class, new classes: smooth and gateDelay

--- ADDAC_Liss.cpp
+++ ADDAC_Liss.cpp
@@ -1,15 +1,18 @@
  
 
 #include "ADDAC_Liss.h"
+//#include "LissSpeed.h"
+
 
 
 //-----------------------------------------------------------------------ADDAC-----------------
+
 ADDAC_Liss::ADDAC_Liss(){	
-	wid = 1023;
-	hei = 1023;
+	ancho = 1023;
+	alto = 1023;
 	
-	AX = wid/2.0f;
-	BX = hei/2.0f;
+	AX = ancho/2.0f;
+	BX = alto/2.0f;
 	
 	a = 1;
 	b= 2;
@@ -22,63 +25,32 @@ ADDAC_Liss::ADDAC_Liss(){
 	
 	Ymin=0.0f;
 	Ymax=1.0f;
-	
-	inc_t	= random(0.0f, 1.0f)/100.0f;
-    inc_t2	= random(0.0f, 1.0f)/100.0f;
-	
-	//Serial.println("LISS COMPLETE");
+	//HITMODE=0;
+	//Serial.println("ULTRALISS COMPLETE");
 }
 
 void ADDAC_Liss::setup(float _inc, float _inc2){
 	inc_t=_inc;
 	inc_t2=_inc;
-	
+	rndF=random(0.0f, 1.0f)/100.0f;
+    rndF2=random(0.0f, 1.0f)/100.0f;
 }
-
 
 
 // --------------------------------------------------------------------------- UPDATE -------------------------
 //
 
-
-void ADDAC_Liss::update(float _min, float _max, float _speedX, float _speedY) {
-    Ymin = _min;
-	Ymax = _max;
-    inc_t=_speedX/8184.0f; // TEMPO MIN = 0.0001 - > TEMPO MAX = 0.25
-	inc_t2=_speedY/8184.0f; // TEMPO MIN = 0.0001 - > TEMPO MAX = 0.25
-	
+void ADDAC_Liss::calcs(float _amp, float _speedx, float _speedy, bool _mirror) {
+    Ymin = 1023*2.0f-_amp*2.0f;
+	Ymax = _amp*2.0f;
+    inc_t=_speedx/1184.0f; // TEMPO MIN = 0.0001 - > TEMPO MAX = 0.25
 	if(inc_t>=1)inc_t=0.999f;
-	else if(inc_t<=0)inc_t=0.0001f;
-    if(inc_t2>=1)inc_t2=0.999f;
-	else if(inc_t2<=0)inc_t2=0.0001f;
-    
-    //int dif=int(Ymax-Ymin+Ymin);
-    
-    x = AX*sin(a*t+delta);
-    y = BX*sin(b*t2);
-    
-    x=x*(_min/1023.0f); //(Ymin/1000.0f);
-    y=y*(_max/1023.0f); //(Ymax/1000.0f);
-    
-    x+=1023/2.0f;
-    y+=1023/2.0f;
+	else if(inc_t<=0)inc_t=0.00001f;
 	
-	x = x / 1023.0f * addacMaxResolution;
-	y = y / 1023.0f * addacMaxResolution;
-	
-    t = t+inc_t;
-    t2 = t2+inc_t2;
-    
-}
-
-
-void ADDAC_Liss::update(float _min, float _max, float _speed) {
-    Ymin = _min;
-	Ymax = _max;
-    inc_t=_speed/8184.0f; // TEMPO MIN = 0.0001 - > TEMPO MAX = 0.25
-	if(inc_t>=1)inc_t=0.999f;
-	else if(inc_t<=0)inc_t=0.0001f;
-    inc_t2=inc_t;
+	inc_t2=_speedy/1184.0f; // TEMPO MIN = 0.0001 - > TEMPO MAX = 0.25
+	if(inc_t2>=1)inc_t2=0.999f;
+	else if(inc_t2<=0)inc_t2=0.00001f;
+    //inc_t2=inc_t;
     
     //int dif=int(Ymax-Ymin+Ymin);
     
@@ -86,17 +58,50 @@ void ADDAC_Liss::update(float _min, float _max, float _speed) {
     x = AX*sin(a*t+delta);
     y = BX*sin(b*t2);
     
-    x=x*(_min/1023.0f); //(Ymin/1000.0f);
-    y=y*(_max/1023.0f); //(Ymax/1000.0f);
+    x=x*(Ymin/1023.0f); //(Ymin/1000.0f);
+    y=y*(Ymin/1023.0f); //(Ymax/1000.0f);
     
     x+=1023/2.0f;
     y+=1023/2.0f;
 	
+	if(!_mirror==0){
+		// COLLISION REFLECTION
+		//Serial.print(" x:");
+		//Serial.print(x);
+		if(x<0)x=-x;//ancho*2-x;
+		else if(x>ancho) x=ancho*2-x;
+		if(y<0) y=-y;
+		else if(y>alto)y=alto*2-y;
+		//print(" |0xy| " + x + " :  " + y + " || ");
+		//Serial.print(" x2:");
+		//Serial.println(x);
+    }else{
+		// WRAP AROUND
+		//Serial.print(" x:");
+		//Serial.print(x);
+		if(x<0)x+=ancho;//ancho*2-x;
+		else if(x>ancho) x-=ancho;
+		if(y<0) y+=alto;
+		else if(y>alto)y-=alto;
+		//print(" |1xy| " + x + " :  " + y + " || ");
+		//Serial.print(" x2:");
+		//Serial.print(x);
+    }
+	
 	x = x / 1023.0f * addacMaxResolution;
 	y = y / 1023.0f * addacMaxResolution;
-	
-    t = t+inc_t;
-    t2 = t2+inc_t2;
+	//x*=addacMaxResolution;
+	//y*=addacMaxResolution;
+    //x=(x*(_max/100.0f))+500-dif/2
+    //x=int(map(x,0,1000,Ymin,Ymax)+Ymin);
+    //y=int(map(y,0,1000,Ymin,Ymax)*(1.0f-Ymin/1000.0f)+Ymin/2);
+    /* Incrementamos el tiempo */
+    //t = t+(inc_t*0.995f)+(spd.getfX()+spd.getfY())/2.0f*0.005f;
+    //t = t+(inc_t*0.9935f)+ spd.getfX()*0.0065f;
+    //inc_t=mouseX/10000.0f+rndF;
+    //inc_t2=mouseY/10000.0f+rndF;
+    t += inc_t/(32.0f-_speedx/32.0f);
+    t2 += inc_t2/(32.0f-_speedy/32.0f);
     
   }
 
--- ADDAC_Liss.h
+++ ADDAC_Liss.h
@@ -1,12 +1,13 @@
 /*
- * ADDAC_liss
- * Version 0.1 March, 2011
+ * ULTRALISS
+ * Version 0.1 November, 2012
+ * Copyright 2009 AndrŽ Gonalves
+ * For details, see http://
+ *
  */
 
 #ifndef ADDAC_Liss_h
 #define ADDAC_Liss_h
-
-#define addacMaxResolution 65535 
 
 #if defined(ARDUINO) && ARDUINO >= 100
 #include "Arduino.h"
@@ -14,16 +15,27 @@
 #include <WProgram.h>
 #endif
 
+// Provides ISR
+#include <avr/interrupt.h>
+
+
+#define addacMaxResolution 65535 
+
+
+
 class ADDAC_Liss{
 public:
 	ADDAC_Liss();
+		void setup(int _width, int _height, float _inc);
 		void setup(float _inc, float _inc2);
+		void setup(float _inc);
+		void update();
+		void update(int _t, int _t2);
 		void setInc(float _inc);
-		void update(float _min, float _max, float _speed);
-		void update(float _min, float _max, float _speedX, float _speedY);
+		void calcs(float _amp, float _speedx, float _speedy, bool _mirror);
 	
 	//LissSpeed Speed;
-	float wid,hei,AX,BX,a,b,x,y;
+	float ancho,alto,AX,BX,a,b,x,y;
 	float delta,t,t2,inc_t,inc_t2;	
 	float rndF,rndF2, Ymin,Ymax,offset;
 	
