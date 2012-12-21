6f2f23faa4443175f2e35494540b814c6cbe2396
joaoFonseca <Joaodafonseca88@gmail.com>
2012-12-21 13:17:19 +0000

    Probabilistic updates

--- examples/TUTORIAL_26_PROBABILISTIC/probabilistic_example/probabilistic_example.ino
+++ examples/TUTORIAL_26_PROBABILISTIC/probabilistic_example/probabilistic_example.ino
@@ -29,39 +29,40 @@ void setup(){
 
 
 void loop(){
+  //update VCC class
   VCC.update();
+  
+  //update Probabilistic class with the following parameters - (A%, B%, C%, D%, OFFSET).
   prb.update(VCC.ReadCv(A,0), VCC.ReadCv(A,1), VCC.ReadCv(A,3), VCC.ReadCv(A,4),0);
 
-  if (VCC.MODE==0){
+      //Switch between glideOn/glideOff with a comparator.
+      // boolean glideCP = cp1.calc(RISE,VCC.ReadManual(A,0),0.5);
+      // if(glideCP) prb.switchGlide();
 
-    if (VCC.SUBMODE==0){
-      
-     boolean randomizeCP = cp.calc(RISE,VCC.ReadCv(A,5),0.5);
-     // boolean glideCP = cp1.calc(RISE,VCC.ReadManual(A,0),0.5);
-     // if(glideCP) prb.switchGlide();
-     prb.setGlide(true);
-         
-      //if(randomizeCP) prb.calc();
-      if(tm.set(VCC.ReadCv(A,5)*1000)) prb.calc();
-      
-      //gate 1
+      //Turn On Glide.
+      prb.setGlide(true); //Turn Off - prb.setGlide(false); 
+
+
+      //Create a timer and store his resut. 
+      boolean timer = tm.set(VCC.ReadCv(A,5)*1000);      
+      //If timer is true, calculate the next value.
+      if(timer) prb.calc();
+
+      //Write gate 1 on outPut 0
       VCC.WriteChannel(0, prb.out1);
-      //gate 2
+      //Write gate 2 on outPut 1
       VCC.WriteChannel(1, prb.out2);
-      //gate 3
+      //Write gate 3 on outPut 2
       VCC.WriteChannel(2, prb.out3);
-      //gate 4
+      //Write gate 4 on outPut 3
       VCC.WriteChannel(3, prb.out4);
-      //gate 5
+      //Write gate 5 on outPut 4
       VCC.WriteChannel(4, prb.out5);
-      
-      //write CVout
+
+      //write CVout on outPut 5
       VCC.WriteChannel(5, prb.CVstream);
-      
-    }
-    
-    
-  }
+
+ 
 
 #ifdef DEBUG
 
@@ -75,14 +76,15 @@ void loop(){
   Serial.print(prb.out4);
   Serial.print("  OUT 5 - ");
   Serial.print(prb.out5);
-  
+
   Serial.print("  CVstream - ");
   Serial.print(prb.CVstream);
-  
+
   Serial.println();
   delay(10);
 #endif
 
 }
+
 
 
