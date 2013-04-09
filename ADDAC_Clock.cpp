69d76118f147c4a6cc9b71b970854816d3797368
joaoFonseca <Joaodafonseca88@gmail.com>
2013-04-09 21:18:24 +0100

    ADDAC_CLOCK_WIP(Buggy)
    work in progress

--- ADDAC_Clock.cpp
+++ ADDAC_Clock.cpp
@@ -7,320 +7,119 @@
 
 #include "ADDAC_Clock.h"
 
-//-----------------------------------------------------------------------ADDAC CLOCK-----------------
+//-----------------------------------------------------------------------ADDAC EMPTY-----------------
 
+ADDAC_Clock CLK;
+// INTERRUPT TIMER 1 - needs no changes!
+ISR(TIMER1_COMPA_vect){//timer1 interrupt 1Hz toggles pin 13 (LED)
 
-ADDAC_Clock::ADDAC_Clock(int _pin){
-	pin=_pin;
-	//Serial.println("OUT SET!");
-	time=millis();
-	oldTime=time;
-	
-	stepsPerBar=4;
-	stepSize=1;
-	actualStep=1;
-	step=1;
-	bars= 1; 
-	actualBar=1;
-	delay=0;
-    Counter=0;
-	
-    Counter=0;
-    gateVal=0;
-    time = 0; 
-    timeInterval=1000; 
-    dutyCycle=100;
-    state = false;
-    CLKtrig=false;
-    CLKval=false;
-    CLKup=false;
-    CLKdown=false;
-    OUT=false;
-
-	STEP=false;
-	ON=false;
-	CLOCK=false;
-}
-
-
-
-ADDAC_Clock::ADDAC_Clock(int _pin, int _steps, int _stepSize){
-	pin=_pin;
-	//Serial.println("simpleOUT SET!");
-	time=millis();
-	oldTime=time;
-	
-	stepsPerBar=_steps;
-	stepSize=_stepSize;
-	actualStep=1;
-	bars= 1; 
-	actualBar=1;
-	delay=0;
-	
-    Counter=0;
-    gateVal;
-    time = 0; 
-    timeInterval=1000; 
-    dutyCycle=100;
-    state = false;
-    CLKtrig=false;
-    CLKval=false;
-    CLKup=false;
-    CLKdown=false;
-    OUT=false;
     
-	STEP=false;
-	ON=false;
-	CLOCK=false;
-}
-
-
-void ADDAC_Clock::checkClock(bool _val){
-    CLKval = _val;
-  #ifdef DEBUG
-    //Serial.print("  TuneTrig:");
-    //Serial.print(CLKval, DEC);
-  #endif
-    // AVOID CONTINUOUS PRESSING
-    if(CLKval && !CLKtrig){ // IF TRIGGER PRESSED
-        CLKtrig=true; // ACTIVE ON
-        CLKup=true;
-    }else if(CLKval && CLKtrig){ // IF PRESSING
-        CLKval=false; // DISABLE
-    }else if(!CLKval && CLKtrig){ // WHEN RELEASED
-        CLKtrig=false; // NOT ACTIVE
-        CLKdown=true;
-    }
-}
-
-void ADDAC_Clock::checkChangeState(){
-    if(CLKup){
-        CLKup=false;
-        state=true;
-        oldTime=time;
-        time=millis();
-        if(!RESEt){
-            timeInterval = time-oldTime;
-        }else{
-            RESEt=false;
-        }
-        Counter++;
-        CLOCK=true;
-        
-    }else if(CLKdown){
-        CLKdown=false;
-        dutyCycle = millis()-time;
-        state = false;
-    }
-}
-
-// --------------------------------------------------------------------------- UPDATE -------------------------
-//
-
-void ADDAC_Clock::update(bool _INval, bool _reset, unsigned long tempTime, int _pot1, int _pot2){
    
-    Pot1Bars = _pot1;
-    Pot2Steps = _pot2;
-    
-    if(_reset){
-        time=millis();
-        reset(time);
-        RESEt=true;
-    }
-    
-	if(CLOCK){
-		clock(tempTime, interval, dutyCycle);
-		CLOCK=false;
-	}
-    
-    //digitalWrite(led0Pin, state);
-    
-    /*if(!state){
-        //digitalWrite(led1Pin, LOW);
-    }else{
-        if(Counter%1==0) //digitalWrite(led1Pin, HIGH);
-    }*/
-    
-    
-    checkClock(_INval);
-    checkChangeState();
-    
-	
-	if (ON && oldTime+dutyCycle  < tempTime) {
-		OUT=false;
-     #ifdef DEBUG
-        Serial.print(" |OFF| ");
-      #endif
-		ON=false;
-	}
-	
-	stepUpdate(tempTime);
-	
-}
 
-void ADDAC_Clock::simpleUpdate(int Counter, int _interval, int _dutyCycle, unsigned long tempTime, int _pot1, int _pot2){
-	
-	//if(_dutyCycle>stepTime*stepSize) dutyCycle=_dutyCycle/stepsPerBar;
-	//else dutyCycle=_dutyCycle;
-	//dutyCycle=50;
-	stepUpdate(tempTime);
-    if(STEP){
-        int mod = (actualStep - 1 + ((actualBar-1)*stepsPerBar))  %  stepSize;
-        if(mod == 0){
-            OUT=true;
-            ON=true;
-            oldTime = tempTime;
-        }
-        STEP=false;
-    }
     
-	if (ON && oldTime+dutyCycle  < tempTime) {
-		ON=false;
-		OUT=false;
-	}
-}
-
-void ADDAC_Clock::stepUpdate(unsigned long tempTime){
-	if(!STEP && time < tempTime && step<=stepsPerBar){
-		time+=stepTime;
-		if(step==1 && actualStep>1)actualStep++;
-		else if(step!=1)actualStep++;
-		
-		step++;
-		
-      #ifdef DEBUG
-		Serial.println();
-		Serial.print(" | step:");
-		Serial.print(step);
-		Serial.print("  |  bars:");
-		Serial.print(bars);
-		Serial.print("  |  actualBar:");
-		Serial.print(actualBar);
-		Serial.print("  |  BarSteps:");
-		Serial.print(stepsPerBar);
-		Serial.print("  |  actualStep:");
-		Serial.print(actualStep);
-		Serial.print("  |  stepSize:");
-		Serial.print(stepSize);
-		//Serial.print("  |  stepTime:");
-		//Serial.print(stepTime);
-		//Serial.print("  |  dCycle:");
-		//Serial.print(dutyCycle);
-      #endif
+    if(CLK.counter++ >CLK.hzToBpm/60){ // add /60 hzToBpm Bad!!!!! -------- ??????
+        // IT'S CLOCK TIME!
         
-		int mod = (actualStep - 1)  %  stepSize;
-        #ifdef DEBUG
-		  Serial.print(" | MODULUS:");
-		  Serial.print(mod);
-        #endif
-		
-        if(mod == 0){
-			//digitalWrite(pin,HIGH);
-			OUT=true;
-            ON=true;
-			oldTime = tempTime;//time-stepTime;
-          #ifdef DEBUG
-			Serial.print("  |ON| ");
-          #endif
-		}
-	}else{
-      #ifdef DEBUG
-		//Serial.print(".");
-      #endif
-	}
-}
+        //CALL FUNCYION FROM ARDUINO
+            CLK.isrCallback();
+//          Serial.println("clock");
+        
+        CLK.counter=0;
+        CLK.ON=true;
+        CLK.CLOCK_Flag=true;
+        //VCC.WriteGatesStraight(A,1,1);
+    }
+    
+//    Serial.print(" CLK.counter - ");
+//    Serial.print(CLK.counter);
+//    Serial.print(" CLK.hzToBpm - ");
+//    Serial.println(CLK.hzToBpm);
 
-void ADDAC_Clock::simpleStepUpdate(unsigned long tempTime){
-	if(!STEP && time+stepTime < tempTime){
-		time+=stepTime;
-		actualStep++;
-		STEP=true;
-	}
+    
+    /*if(counter >hzToBpm/2){
+     // IT'S CLOCK TIME!
+     counter=0;
+     ON=false;
+     VCC.WriteGatesStraight(A,1,0);
+     }*/
 }
 
 
 
 
-void ADDAC_Clock::reset(unsigned long _time){
-	step=1;
-	time=_time;
-	actualBar=1;
-	actualStep=1;
-	stepSize = Pot1Bars/1024.0f*(stepsPerBar*2)+1; // prevent divide by zero!!
-	
-	for (int i = 1; i<50; i++) {
-		int temp = (stepSize * i) % stepsPerBar;
-		if(temp == 0){
-			bars = i;
-			break;
-		}
-	}
-	stepUpdate(_time);
-}
 
-void ADDAC_Clock::clock(unsigned long _time, int _interval, int _dutyCycle){
-	step=1;
-	time=_time;
-	stepsPerBar = Pot2Steps/1023.0f*12; // prevent divide by zero!!
-	stepTime = _interval/stepsPerBar;
-	stepSize = Pot1Bars/1024.0f*(stepsPerBar*2)+1; // prevent divide by zero!!
+ADDAC_Clock::ADDAC_Clock(){	// INITIALIZE CLASS
 	
-	dutyCycle=stepTime*stepSize/2;
-	
-	if(stepSize>1){
-		int div=0;
-		for (int i = 1; i<50; i++) {
-			div = (stepsPerBar-stepSize + div) % stepSize;
-			if(div == 0){
-				bars = i;
-				break;
-			}
-		}
-	}else{
-		bars=1;
-		
-	}
-	
-	if(actualBar < bars){
-		actualBar++;
-		stepUpdate(_time);
-	}else {
-		actualBar=1;
-		actualStep=1;
-	}
-}
-
-void ADDAC_Clock::simpleClock(unsigned long _time, int _interval, int _dutyCycle){
-	time=_time;
-	oldTime=time;
-	STEP=true;
-
-	stepTime = _interval/stepsPerBar;
-
-	dutyCycle=stepTime*stepSize/2;
-	if(stepSize>1){
-		int div=0;
-		for (int i = 1; i<50; i++) {
-			div = (stepsPerBar-stepSize + div) % stepSize;
-			if(div == 0){
-				bars = i;
-				break;
-			}
-		}
-	}else{
-		bars=1;
-		
-	}
-	
-	if(actualBar < bars){
-		actualBar++;
-	}else {
-		actualBar=1;
-		actualStep=1;
-	}
 }
 
 
+void ADDAC_Clock::setup(void (*isr)()){	// setup CLASS
+    
+    BPM = 120;
+    gateOnTime = 50; //milliseconds for gate on time
+    CLOCK_Flag = false;
+    // other variables needed for the interrupt timer
+    counter = 0;
+    hzToBpm = CPU_CLOCK / BPM;
+    ON=false;
+    Time=0;
+    
+    //FOR CALLBACK
+    isrCallback = isr;                                       // register the user's callback with the real ISR
+    
+    //Set TIMER
+    // TIMER INTERRUPTS - DO NOT CHANGE THIS CHUNK!
+    cli();//stop interrupts
+    //set timer1 interrupt at 1Hz
+    TCCR1A = 0;// set entire TCCR1A register to 0
+    TCCR1B = 0;// same for TCCR1B
+    TCNT1  = 0;//initialize counter value to 0
+    // set compare match register for 1hz increments
+    OCR1A = 124; //  124 = 7500bpm <=> 125HZ <=> (16*10^6) / (125*1024) - 1 (must be <65536)
+    //OCR1A = 24; // 24 = 37500bpm <=> 625HZ <=> (16*10^6) / (625*1024) - 1 (must be <65536)
+    // turn on CTC mode
+    TCCR1B |= (1 << WGM12);
+    // Set CS12 and CS10 bits for 1024 prescaler
+    TCCR1B |= (1 << CS12) | (1 << CS10);
+    // enable timer compare interrupt
+    TIMSK1 |= (1 << OCIE1A);
+    sei();//allow interrupts
+    
+	setBPM(120);
 
-// --------------------------------------------------------------------------- END ----------------------------------
-//
+}
+
+
+void ADDAC_Clock::update(){	// update CLASS
+    
+    
+    
+    if(CLOCK_Flag){
+        CLOCK_Flag=false;
+       // VCC.WriteGatesStraight(A,1,1);
+        Time = millis();
+    }
+    
+    if(Time + gateOnTime <millis()){
+        //VCC.WriteGatesStraight(A,1,0);
+    }
+	
+    
+}
+
+
+void ADDAC_Clock::setBPM(int _bpm){	// set BPM CLASS
+	
+    
+   // int tempBPM = int(VCC.ReadManual(A,0)*500+1); // Manual 1 controls from 1 to 501 BPMs
+    int tempBPM=_bpm;
+    if(BPM != tempBPM){
+        hzToBpm = CPU_CLOCK / BPM;
+        BPM=tempBPM;//add this???
+        counter=0;
+    }
+    
+    
+}
+
+
--- ADDAC_Clock.h
+++ ADDAC_Clock.h
@@ -1,30 +1,6 @@
 /*
- * ADDAC_Clock
+ * ADDAC_Clock - - - > WORK ON PROGRESS - still buggy!!!
  * 
- * Developer's blank page Class example
- *
- * in Arduino environment simply add at the very top:
- 
- #include <ADDAC_Clock.h> // This loads the Class when compiling
- ADDAC_Clock Empty; // This Instantiates 1 Class with the name "Empty"
- 
- *
- *
- * then in BEHAVIOUR() add at a position of your choice:
- 
- unsigned int CV = Empty.update(random(0, 65535));
- 
- or if you want to write it straight out at channel 1 for ex.:
- VCC.WriteChannel(1, Empty.update(random(0, 65535)) );
- 
- *
- *
- * or to simply get the current value back use:
- 
- unsigned int CV = Empty.CVstream;
- 
- *
- *
  */
 
 #ifndef ADDAC_Clock_h
@@ -36,50 +12,33 @@
 #include <WProgram.h>
 #endif
 
-#define addacMaxResolution 65535 
 
-#define DEBUG
+#define CPU_CLOCK 7500 // 125Hz
+
 
 class ADDAC_Clock{
+    
 public:
-	ADDAC_Clock(int _pin);
-	ADDAC_Clock(int _pin, int _steps, int _stepSize);
-	void update(bool _INval, bool _reset, unsigned long tempTime, int _pot1, int _pot2);
-    void simpleUpdate(int Counter, int _interval, int _dutyCycle, unsigned long tempTime, int _pot1, int _pot2);
     
-	void reset(unsigned long _time);
-	void clock(unsigned long _time, int _interval, int _dutyCycle);
-	void simpleClock(unsigned long _time, int _interval, int _dutyCycle);
-	
-    void stepUpdate(unsigned long tempTime);
-    void simpleStepUpdate(unsigned long tempTime);
-	
-    void checkClock(bool _val);
-    void checkChangeState();
+    int BPM;
+    int gateOnTime; //milliseconds for gate on time
+    bool CLOCK_Flag;
+    int counter;
+    int hzToBpm;
+    bool ON;
+    unsigned long Time;
     
-	unsigned long time, oldTime;
-	int pin;
-	bool STATE;
-	
-	
-	int dutyCycle, stepTime;
-	int stepsPerBar, stepSize, actualStep, step; 
-	int bars, actualBar;
-	int delay;
-	bool STEP; 
-    bool ON; 
-    bool CLOCK;
-    bool OUT;
+    ADDAC_Clock();
     
+    void setup(void (*isr)());
+    void update();
+    void setBPM(int _bpm);
+    void (*isrCallback)();
     
-    bool RESEt;
-    
-    int Counter, Change;
-    int Pot1Bars, Pot2Steps;
-    int gateVal;
-    bool CLKtrig, CLKval, CLKup, CLKdown;
-    int timeInterval, interval;
-    int state;
+   
 	
 };
+
+extern ADDAC_Clock CLK;
+
 #endif
