40d916c39c8a4cf124c2d2d96a49c592591318b2
joaoFonseca <Joaodafonseca88@gmail.com>
2013-05-10 14:53:03 +0100

    AD example updated

--- examples/TUTORIAL_11_ENVELOPES/AD_example/AD_example.ino
+++ examples/TUTORIAL_11_ENVELOPES/AD_example/AD_example.ino
@@ -8,6 +8,7 @@
 ADDAC VCC; 
 
 #include <ADDAC_Comparator.h>
+ADDAC_Comparator cp, cp1;
 
 #include <ADDAC_AD.h>
 ADDAC_AD ad; 
@@ -39,10 +40,32 @@ void loop(){
 
     gate =false;
   }
+  
+  
+  
 
+  //SHAPE AD WITH GATE
+  if(VCC.MODE==0){
 
-  ad.updateLogExpMode(gate,1,700,0.7,900,0.3);
-  //ad.updateLin(gate,700,700);
+    ad.updateLogExpMode(gate,1,700,0.7,900,0.3);
+  }
+
+  //SHAPE AD WITHOU GATE
+  else if(VCC.MODE==1){
+
+    //TRIGGER OR REALEASE GATE WITH COMPARATORS
+    if(cp.calc(RISE,VCC.ReadCv(A,0),0.5))ad.trigger();
+    else if(cp1.calc(RISE,VCC.ReadCv(A,1),0.5))ad.release();
+
+    ad.updateLogExpMode(1,700,0.7,900,0.3);
+  }
+
+  //LINEAR AD WITH GATE
+  else if(VCC.MODE==2){
+
+    ad.updateLin(gate,700,700);
+  }
+
 
 
   VCC.WriteChannel(0,ad.CVstream);
@@ -55,6 +78,8 @@ void loop(){
 #endif
 
 }
+
+
 
 
 
